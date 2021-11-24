5bdd27986f12baa98694fa139447efa37d7651d1
barslev <benjaminbarslev@gmail.com>
2021-11-24 12:15:23 +0100

    Added small project with a dummy component file that uses lodash and rxjs

---
+++ src/some-component/some.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import _ from 'lodash';
+import rxjs from 'rxjs'
+
+@Component({
+    selector: 'some-component',
+    templateUrl: './some.component.html',
+    styleUrls: ['./some.component.css'],
+})
+export class SomeComponent {
+    _ = _;
+    subject = new rxjs.Subject();
+    constructor() {}
+
+    private f() {
+        this._.pluck([1, 2, 3], x => x);
+        this.subject.filter(x => true).map(x => x);
+    }
+}
