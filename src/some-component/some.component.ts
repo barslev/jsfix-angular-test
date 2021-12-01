5533faeef8f0dc48a4194a4ed496cc74b58d777d
barslev <benjaminbarslev@gmail.com>
2021-12-01 10:20:04 +0100

    Added more rxjs and uuid affected locations

--- src/some-component/some.component.ts
+++ src/some-component/some.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import _ from 'lodash';
-import rxjs from 'rxjs'
+import rxjs from 'rxjs';
+import uuid from 'uuid';
 
 @Component({
     selector: 'some-component',
@@ -10,10 +11,12 @@ import rxjs from 'rxjs'
 export class SomeComponent {
     _ = _;
     subject = new rxjs.Subject();
+    uuid = uuid;
     constructor() {}
 
     private f() {
-        this._.pluck([1, 2, 3], x => x);
-        this.subject.filter(x => true).map(x => x);
+        _.pluck([1, 2, 3], (x) => x);
+        this._.pluck([1, 2, 3], (x) => x);
+        this.subject.filter((x) => true).map((x) => x);
     }
 }
